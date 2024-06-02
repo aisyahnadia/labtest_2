1f4acd0d9588f4e55aad4d328ca6f03263ec6e4b
aisyahnadia <76759084+aisyahnadia@users.noreply.github.com>
2024-06-02 19:33:46 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <NavbarComponent /> 
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavbarComponent from './components/NavigationBar.vue';
+import '@/assets/css/styles.css';
+export default {
+  name: 'App',
+  components: {
+    NavbarComponent
+  },
+};
+</script>
+
+<style>
+
+.main {
+  width: 100%;
+  background-color: #000;
+  background-image: url("@/assets/bg_image.png"); 
+  background-position: center;
+  background-size: cover;
+  height: 100vh;
+}
+</style>
+
+
