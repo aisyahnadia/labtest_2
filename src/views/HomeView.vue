dfb9634e06d133fc1b100ce82f251b52314c1a83
aisyahnadia <76759084+aisyahnadia@users.noreply.github.com>
2024-06-02 19:40:17 +0800

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div >
+    <ContentComponent />
+  </div>
+</template>
+
+<script>
+import ContentComponent from "@/components/ContentArea";
+
+export default {
+  name: "HomeView",
+  components: {
+    ContentComponent,
+  },
+};
+</script>
